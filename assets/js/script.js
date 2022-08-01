73eb553de408fc22bdf7a304b27e908fa98dd56c
Chris Nowicki <chris@chrisnowicki.io>
2022-08-01 10:53:27 -0400

    updated ajax fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,19 @@
 // function to get information from The Bored API
-function randomActivity() {    
+
+function getActivity() {
     fetch("https://www.boredapi.com/api/activity?type=" + document.getElementById("activity").value)
-        .then((response) => response.json())
-        .then((data) => {
-            // creates an activityDisplay variable to assign to the <span> tag that the 
-            let activityDisplay = document.getElementById("activity-display")
-            
-            // if user does not select an activity type and selects the go button then the innerHTML will display the "Please select activity" message
-            // if we get a return from the API then it displays the activity object from The Bored API
-            if (data.activity === undefined) {
-                activityDisplay.innerHTML = "{ Please select and activity from the drop-down menu above! }"
-            } else {
-                activityDisplay.innerHTML = data.activity
-            }
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
+    .then((response) => response.json())
+    .then((data) => {
+        // creates an activityDisplay variable to assign to the <span> tag that the 
+        let activityDisplay = document.getElementById("activity-display")
+        
+        // if user does not select an activity type and selects the go button then the innerHTML will display the "Please select activity" message
+        // if we get a return from the API then it displays the activity object from The Bored API
+        if (data.activity === undefined) {
+            activityDisplay.innerHTML = "{ Please select and activity from the drop-down menu above! }"
+        } else {
+            activityDisplay.innerHTML = data.activity
+        }
+    })
+    .catch(err => console.error(err));
 }

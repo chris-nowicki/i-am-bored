31789033882c953dca46057361daf8137f815a80
Chris Nowicki <chris@chrisnowicki.io>
2022-08-01 11:34:05 -0400

    removed https from fetch url

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // function to get information from The Bored API
 
 function getActivity() {
-    fetch("https://www.boredapi.com/api/activity?type=" + document.getElementById("activity").value)
+    fetch("http://www.boredapi.com/api/activity?type=" + document.getElementById("activity").value)
     .then((response) => response.json())
     .then((data) => {
         // creates an activityDisplay variable to assign to the <span> tag that the 
